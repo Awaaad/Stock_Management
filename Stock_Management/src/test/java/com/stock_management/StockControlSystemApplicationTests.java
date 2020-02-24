27134f03be30cad3e6c949b1e2131dbfd67aac79
Awaaad <awad_luckhoo@hotmail.com>
2020-02-24 11:30:30 +0400

    Add files via upload

---
+++ Stock_Management/src/test/java/com/stock_management/StockControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
