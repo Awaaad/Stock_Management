b336564ce1e3bc6c6cd2dda3398304ea318a018f
Awad_lk <muhammad.awad.luckhoo@elca.mu>
2020-12-24 23:22:13 +0400

    x

--- Stock_Management/src/main/java/com/stock_management/service/implementation/RoleServiceImplementation.java
+++ Stock_Management/src/main/java/com/stock_management/service/implementation/RoleServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.stock_management.service.implementation;public class RoleServiceImplementation {
+package com.stock_management.service.implementation;
+
+import com.stock_management.dto.RoleDto;
+import com.stock_management.entity.Role;
+import com.stock_management.mapper.RoleMapper;
+import com.stock_management.repository.RoleRepository;
+import com.stock_management.service.RoleService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RoleServiceImplementation implements RoleService {
+    public final RoleRepository roleRepository;
+    public final RoleMapper roleMapper;
+
+    public RoleServiceImplementation(RoleRepository roleRepository, RoleMapper roleMapper) {
+        this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
+    }
+
+    @Override
+    public void saveRole(RoleDto roleDto) {
+        var saveRoleInformation = roleMapper.mapRoleDtoToEntity(roleDto);
+        roleRepository.save(saveRoleInformation);
+    }
+
+    @Override
+    public List<RoleDto> findAllRoles() {
+        List<Role> roles = roleRepository.findAll();
+        return roles.stream().map(roleMapper::mapRoleEntityToDto).collect(Collectors.toList());
+    }
 }
--- Stock_Management/src/main/java/com/stock_management/service/implementation/UserRoleDetailsServiceImplementation.java
+++ Stock_Management/src/main/java/com/stock_management/service/implementation/UserRoleDetailsServiceImplementation.java
@@ -1,2 +1,31 @@
-package com.stock_management.service.implementation;public class UserRoleDetailsServiceImplementation {
-}
+//package com.stock_management.service.implementation;
+//
+//import com.stock_management.entity.User;
+//import com.stock_management.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Objects;
+//
+//@Service
+//public class UserRoleDetailsServiceImplementation implements UserDetailsService {
+//
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User user = userRepository.findByUsername(username);
+//        UserRoleDetails userRoleDetails;
+//        if (Objects.nonNull(user)) {
+//            userRoleDetails = new UserRoleDetails();
+//            userRoleDetails.setUser(user);
+//        } else {
+//            throw new UsernameNotFoundException("User does not exist");
+//        }
+//        return userRoleDetails;
+//    }
+//}
