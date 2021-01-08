579295f8d855ceebb1881774a1173a59d7f3a566
Awad_lk <muhammad.awad.luckhoo@elca.mu>
2021-01-08 23:00:31 +0400

    x

--- Stock_Management/src/main/java/com/stock_management/StockControlSystemApplication.java
+++ Stock_Management/src/main/java/com/stock_management/StockControlSystemApplication.java
@@ -9,7 +9,6 @@ import com.stock_management.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
