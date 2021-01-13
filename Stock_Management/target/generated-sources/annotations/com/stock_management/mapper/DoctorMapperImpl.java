69e8edb1bd5e4600d0e6a3435a547dc56520b6cf
Awad_lk <muhammad.awad.luckhoo@elca.mu>
2021-01-13 23:09:33 +0400

    prod

--- Stock_Management/target/generated-sources/annotations/com/stock_management/mapper/DoctorMapperImpl.java
+++ Stock_Management/target/generated-sources/annotations/com/stock_management/mapper/DoctorMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-13T22:31:22+0400",
+    date = "2021-01-13T23:07:24+0400",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.6 (AdoptOpenJDK)"
 )
 @Component
--- Stock_Management/target/generated-sources/annotations/com/stock_management/mapper/ReceiptMapperImpl.java
+++ Stock_Management/target/generated-sources/annotations/com/stock_management/mapper/ReceiptMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-13T22:31:21+0400",
+    date = "2021-01-13T23:07:23+0400",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.6 (AdoptOpenJDK)"
 )
 @Component
